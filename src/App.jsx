a5e0037279e33c9bb155bdc390e2b1c5feaa5ff8
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-16 11:13:06 +0000

    main styles added

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ function App() {
 
   return (
     <div className="App">
-      test
+      <p className='hero-text'>Bir Peri Masali</p>
+
+      <div className='light-container'>
+        <h1>h1 - Main Title</h1>
+        <h2>h2 - Subtitle</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, facere exercitationem doloremque hic dolorum iste, aperiam expedita, repellat tempore eius quos. Quo cum fuga iure assumenda numquam error rerum id <a href='#link'>link</a>.</p>
+      </div>
     </div>
   )
 }
