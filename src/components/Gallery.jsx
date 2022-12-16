e72e1170bf7c0b648de5b7c1601ba1ffc25a2afd
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-16 15:13:09 +0000

    images added for slider gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,8 +4,6 @@ const Gallery = () => {
   return (
     <div id='galeri' className='gray-container'>
         <h1>Galeri</h1>
-        <h2>Randevu talepleri ve diger tum sorulariniz icin</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, debitis itaque. Aliquid earum incidunt amet doloremque, excepturi itaque quidem similique a atque quam error molestiae repellendus qui unde commodi vel.</p>
     </div>
   )
 }
