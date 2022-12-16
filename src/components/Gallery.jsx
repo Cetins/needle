391b1b7bddd64f19408aa329d1a02e0c816d4ce7
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-16 14:28:16 +0000

    gallery bg changed to mid-gray, pink didn't worked

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Gallery = () => {
   return (
-    <div id='galeri' className='light-container'>
+    <div id='galeri' className='gray-container'>
         <h1>Galeri</h1>
         <h2>Randevu talepleri ve diger tum sorulariniz icin</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga, debitis itaque. Aliquid earum incidunt amet doloremque, excepturi itaque quidem similique a atque quam error molestiae repellendus qui unde commodi vel.</p>
