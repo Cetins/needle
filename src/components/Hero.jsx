3ff0de94a048ca0394afbd404fc962eeb2b869c6
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-16 16:32:15 +0000

    hero alignment changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div id='hero' className='hero-div'>
+    <div className='hero-div'>
       <svg viewBox="0 0 240 80" xmlns="http://www.w3.org/2000/svg">
-        <text x="20" y="35" className="bir">Bir</text>
+        <text x="10" y="35" className="bir">Bir</text>
         <text x="40" y="35" className="peri">Peri</text>
-        <text x="65" y="55" className="masali">Masali!</text>
+        <text x="75" y="60" className="masali">Masali!</text>
       </svg>
     </div>
   )
