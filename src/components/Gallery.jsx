bf25d887d68ead9a6fa1587f5f39e0fa9474881f
Cetins <69481095+Cetins@users.noreply.github.com>
2022-12-16 15:50:34 +0000

    gallery component completed

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import SimpleImageSlider from "react-simple-image-slider";
 
 const Gallery = () => {
+  const images = [
+    { url: "src/assets/gallery/1.jpg" },
+    { url: "src/assets/gallery/2.jpg" },
+    { url: "src/assets/gallery/3.jpg" },
+    { url: "src/assets/gallery/4.jpg" },
+    { url: "src/assets/gallery/5.jpg" }
+  ];
   return (
-    <div id='galeri' className='gray-container'>
+    <div id='galeri' className='gallery-container'>
         <h1>Galeri</h1>
+        <div>
+          <SimpleImageSlider
+            id={"slider"}
+            width={"60rem"}
+            height={"30rem"}
+            images={images}
+            showBullets={true}
+            showNavs={true}
+          />
+        </div>
     </div>
   )
 }
